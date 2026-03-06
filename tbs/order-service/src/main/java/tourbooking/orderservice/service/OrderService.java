e9ba005258f9e1b9dd77b37314a55d5a19932d94
Yegor <egorkhary@mail.ru>
2026-03-06 13:53:33 +0300

    ADD: create service for orders

---
+++ tbs/order-service/src/main/java/tourbooking/orderservice/service/OrderService.java
@@ -0,0 +1,25 @@
+package tourbooking.orderservice.service;
+
+import org.springframework.stereotype.Service;
+
+import reactor.core.publisher.Mono;
+import tourbooking.orderservice.domain.Order;
+import tourbooking.orderservice.dto.CreateOrderRequest;
+import tourbooking.orderservice.repository.OrderRepository;
+
+@Service
+public class OrderService {
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public Mono<Order> createOrder(CreateOrderRequest request) {
+        Order order = new Order(
+                request.getUserId(),
+                request.getFlightId(),
+                request.getHotelId());
+        return orderRepository.save(order);
+    }
+}
