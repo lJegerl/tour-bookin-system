4e6cfc5b7d07721f3329d6c31682bd964712758c
Yegor <egorkhary@mail.ru>
2026-03-10 17:13:26 +0300

    FIX: add some logs

--- tbs/order-service/src/main/java/tourbooking/orderservice/OrderServiceApplication.java
+++ tbs/order-service/src/main/java/tourbooking/orderservice/OrderServiceApplication.java
@@ -1,13 +1,22 @@
 package tourbooking.orderservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner testMongo(MongoTemplate template) {
+        return args -> System.out.println("Mongo connection ok: " + template.getDb().getName());
+    }
 }
