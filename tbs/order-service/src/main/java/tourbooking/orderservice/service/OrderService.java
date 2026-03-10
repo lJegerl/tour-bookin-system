13c355b1539dc60ac4b156c4e178b357b51f848b
Yegor <egorkhary@mail.ru>
2026-03-10 17:14:29 +0300

    FIX: add some logs

--- tbs/order-service/src/main/java/tourbooking/orderservice/service/OrderService.java
+++ tbs/order-service/src/main/java/tourbooking/orderservice/service/OrderService.java
@@ -2,6 +2,7 @@ package tourbooking.orderservice.service;
 
 import org.springframework.stereotype.Service;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import tourbooking.orderservice.domain.Order;
 import tourbooking.orderservice.dto.CreateOrderRequest;
@@ -20,10 +21,20 @@ public class OrderService {
                 request.getUserId(),
                 request.getFlightId(),
                 request.getHotelId());
-        return orderRepository.save(order);
+
+        return orderRepository.save(order)
+                .doOnSubscribe(s -> System.out.println("SUBSCRIBE"))
+                .doOnNext(o -> System.out.println("Saved order: " + o.getId()))
+                .doOnSuccess(o -> System.out.println("SUCCESS"))
+                .doOnError(e -> e.printStackTrace());
     }
 
     public Mono<Order> getOrder(String id) {
-        return orderRepository.findById(id);
+        return orderRepository.findById(id)
+                .switchIfEmpty(Mono.error(new RuntimeException("Order not found")));
+    }
+
+    public Flux<Order> getOrders() {
+        return orderRepository.findAll();
     }
 }
