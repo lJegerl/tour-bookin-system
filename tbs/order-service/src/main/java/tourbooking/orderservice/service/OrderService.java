03e46c70ad9a70e65abe9e9056ba13db51f310ee
Yegor <egorkhary@mail.ru>
2026-03-11 17:11:55 +0300

    ADD: create event method

--- tbs/order-service/src/main/java/tourbooking/orderservice/service/OrderService.java
+++ tbs/order-service/src/main/java/tourbooking/orderservice/service/OrderService.java
@@ -6,27 +6,41 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import tourbooking.orderservice.domain.Order;
 import tourbooking.orderservice.dto.CreateOrderRequest;
+import tourbooking.orderservice.events.OrderCreatedEvent;
+import tourbooking.orderservice.messaging.OrderEventPublisher;
 import tourbooking.orderservice.repository.OrderRepository;
 
 @Service
 public class OrderService {
-    private final OrderRepository orderRepository;
 
-    public OrderService(OrderRepository orderRepository) {
+    private final OrderRepository orderRepository;
+    private final OrderEventPublisher publisher;
+
+    public OrderService(OrderRepository orderRepository,
+            OrderEventPublisher publisher) {
         this.orderRepository = orderRepository;
+        this.publisher = publisher;
     }
 
     public Mono<Order> createOrder(CreateOrderRequest request) {
+
         Order order = new Order(
                 request.getUserId(),
                 request.getFlightId(),
                 request.getHotelId());
 
         return orderRepository.save(order)
-                .doOnSubscribe(s -> System.out.println("SUBSCRIBE"))
-                .doOnNext(o -> System.out.println("Saved order: " + o.getId()))
-                .doOnSuccess(o -> System.out.println("SUCCESS"))
-                .doOnError(e -> e.printStackTrace());
+                .doOnSuccess(saved -> {
+
+                    OrderCreatedEvent event = new OrderCreatedEvent(
+                            saved.getId(),
+                            saved.getUserId(),
+                            saved.getFlightId(),
+                            saved.getHotelId());
+
+                    publisher.publishOrderCreated(event);
+
+                });
     }
 
     public Mono<Order> getOrder(String id) {
